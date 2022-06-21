ed53a708a9fd500ccf3c04219a1016023bedd9ad
musleh0001 <khan.muslehuddin0001@gmail.com>
2022-06-21 17:40:11 +0600

    feat: useState to manage state

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,28 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useContext } from "react";
+import GithubContext from "../../context/github/GithubContext";
 
 import Spinner from "../layout/Spinner";
 import UserItem from "./UserItem";
 
 const UserResults = () => {
-	const [users, setUsers] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
+	const { users, loading, fetchUsers } = useContext(GithubContext);
 
 	useEffect(() => {
 		fetchUsers();
 	}, []);
 
-	const fetchUsers = async () => {
-		const response = await fetch(`${import.meta.env.VITE_GITHUB_URL}/users`, {
-			headers: {
-				Authorization: `token ${import.meta.env.VITE_GITHUB_TOKEN}`,
-			},
-		});
-		const data = await response.json();
-		setUsers(data);
-		setIsLoading(false);
-	};
-
-	if (!isLoading) {
+	if (!loading) {
 		return (
 			<div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
 				{users.map((user) => (
